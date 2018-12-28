ae74b8f2018fac91100fa81c0d52049497d02ef5
Александр Ведров <sanemwow@gmail.com>
2018-12-28 20:44:24 +0500

    123

--- asi_express/CodedUITest1.cs
+++ asi_express/CodedUITest1.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 
 namespace asi_express
 {
-    //try to check_commit
+    //try to check_commit22
     //    Dictionary Dict 
     // Комментарии к параметрам методов
     // WaC - коэффициент ожидания в зависимости от  оракла/мсскл
